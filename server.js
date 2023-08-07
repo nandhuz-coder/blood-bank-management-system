ee4892212d5dc83c93b34c2973a26f7d157f836d
codezen28 <128896831+codezen28@users.noreply.github.com>
2023-08-07 15:12:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,14 +27,21 @@ app.use("/api/v1/inventory",require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics",require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin",require("./routes/adminRoutes"));
 
-//Static folder
-app.use(express.static(path.join(__dirname,'./client/build')));
+const __dirname1=path.resolve();
+if(process.env.NODE_ENV === "production")
+{
+  app.use(express.static(path.join(__dirname1,"/client/build")));
 
-//Static Routes
-app.get("*",function(req,res){
-  res.sendFile(path.join(__dirname,"./client/build/index.html"));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname1,"client","build","index.html"));
+})
+}
+else{
+  app.get("/", (req, res) => {
+  res.send("API is running successfully");
+    
 });
-
+}
 //port
 const PORT = process.env.PORT || 8080;
 
